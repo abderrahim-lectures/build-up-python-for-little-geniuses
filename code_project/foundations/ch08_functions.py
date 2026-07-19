392787f64339529a92e51172f7ec575029ca8ae1
Abderrahim Adrabi <abderrahim.adrabi@gmail.com>
2026-07-19 08:26:38 +0100

    Chapter 8: Functions (#31)
    Closes #30

---
+++ code_project/foundations/ch08_functions.py
@@ -0,0 +1,6 @@
+"""Chapter 8: Functions. A math formula like <code>area = length * width</code> takes inputs and produces an output – a Python function is exactly that: input in, answer out."""
+
+
+def area(length: float, width: float):
+    """Return the area of a rectangle."""
+    return length * width
